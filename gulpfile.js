b3f23536e15900338de9f98190b0cc49f5ed5037
retwedt <retwedt@gmail.com>
2016-05-08 13:41:28 -0500

    only include packages necessary for building sass
    this is for the dist version of this project, only need to include
    packages for building sass

--- gulpfile.js
+++ gulpfile.js
@@ -7,27 +7,10 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 var autoprefixer = require("gulp-autoprefixer");
-var uglify = require("gulp-uglify"); // js minifier
-var rename = require("gulp-rename");
-var merge = require("merge-stream");
-var ghPages = require("gulp-gh-pages");
 
 
 // Build Tasks
 // ***********
-
-// minify and copy JS for the project into dist/
-gulp.task("js-min", function() {
-	return gulp.src("src/js/*.js")
-		.pipe(uglify())
-		.pipe(rename({suffix: ".min"}))
-		.pipe(gulp.dest("dist"))
-});
-gulp.task("js", function() {
-	return gulp.src("src/js/*.js")
-		.pipe(gulp.dest("dist"))
-});
-
 
 // Convert from sass to css, add vendor prefixes
 gulp.task("sass", function () {
@@ -57,47 +40,7 @@ gulp.task("sass", function () {
 });
 
 
-// Copy slide show image assets to the dist folder
-gulp.task("img", function () {
-	return img = gulp.src("src/img/*.*")
-		.pipe(gulp.dest("dist/img"));
-});
-
-
-// build
-gulp.task("build", [
-	"js-min",
-	"js",
-	"sass",
-	"img"
-]);
-
-
 // Default task, build the library
 gulp.task("default", [
-	"build"
+	"sass"
 ]);
-
-
-
-// Deploy Tasks
-// ************
-
-// create a live-demo folder for github pages
-gulp.task("publish", function () {
-  var html = gulp.src("./src/demo/**/*.*")
-  	.pipe(gulp.dest("./_demo"));
-  var dist = gulp.src("./dist/**/*.*")
-    .pipe(gulp.dest("./_demo/dist"));
-  return merge(html, dist);
-});
-
-
-// deploy to github pages
-gulp.task("deploy", ["publish"], function () {
-  return gulp.src("./_demo/**/*")
-	.pipe(ghPages({
-		remoteUrl: "https://github.com/retwedt/theater-lightbox.git"
-	}));
-});
-
